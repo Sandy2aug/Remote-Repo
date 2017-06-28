452c8e861090a161abfbaa37c955e12a3d498366
sandeep <sandeep@sandeep-PC>
2017-06-28 23:21:41 +0530

    Initial Checkin

---
+++ StringComparison/src/com/strings/A.java
@@ -0,0 +1,13 @@
+package com.strings;
+
+public final class A {
+	public static String s;
+	public String s1;
+	
+	A()
+	{
+		s=new String("atest");
+		s1=new String("btest");
+	}
+
+}
---
+++ StringComparison/src/com/strings/EqualityCheck.java
@@ -0,0 +1,18 @@
+package com.strings;
+
+public class EqualityCheck {
+
+	public static void main(String[] args) {
+		A a=new A();
+		A a1=new A();
+		
+		System.out.println(a.s);
+		System.out.println(a1.s);
+		System.out.println(a.s==a1.s);
+		
+		System.out.println(a.s1);
+		System.out.println(a1.s1);
+		System.out.println(a.s1==a1.s1);
+	}
+
+}
